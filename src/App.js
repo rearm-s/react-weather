89724d37bed83b07f4fe1110f4c4c9054f89b125
Vladyslav <rearm8@gmail.com>
2021-11-05 00:08:02 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import {Input} from "./Input";
 import {CardList} from "./CardList";
 import {useCitiesList} from "./hooks/useCitiesList";
 import {createContext} from "react";
-import {BrowserRouter, Route} from "react-router-dom";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 import {SingleCity} from "./SingleCity";
 
 export const GlobalContext = createContext();
@@ -16,13 +16,15 @@ function App() {
         <BrowserRouter>
             <GlobalContext.Provider value={{state, dispatch}}>
                 <div className="Main">
-                    <Route exact path="/">
-                        <Input/>
-                        <CardList/>
-                    </Route>
-                    <Route path="/city/:city">
-                        <SingleCity/>
-                    </Route>
+                    <Switch>
+                        <Route exact path="/">
+                            <Input/>
+                            <CardList/>
+                        </Route>
+                        <Route path="/city/:city">
+                            <SingleCity/>
+                        </Route>
+                    </Switch>
                 </div>
             </GlobalContext.Provider>
         </BrowserRouter>
